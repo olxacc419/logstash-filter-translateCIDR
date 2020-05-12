0570fd19f3660b825d291ea8af4511ca55bf2a84
lsoumille <lucas.soumille@etu.unice.fr>
2020-05-12 14:34:12 +0200

    sort dictionary based on value instead of key

--- lib/logstash/filters/fetch_strategy/file.rb
+++ lib/logstash/filters/fetch_strategy/file.rb
@@ -37,8 +37,8 @@ module LogStash module Filters module FetchStrategy module File
     def dictionary_updated
       @keys_cidr.clear
       @dictionary.keys.each{|k| @keys_cidr[k] = IPAddr.new(k)}
-      #Sort dictionnary
-      @keys_cidr = @keys_cidr.sort.to_h
+      #Sort dictionnary by value
+      @keys_cidr = @keys_cidr.sort_by {|k,v| v}.to_h
     end
 
     def binary_search_recursive(ip_hash, ip)
@@ -58,11 +58,15 @@ module LogStash module Filters module FetchStrategy module File
               return ip_array[i]
           else
               comp_result = ip<=>ip_hash[ip_array[i]]
+              @sub_hash_key = Hash.new()
               if comp_result == 1
-                  return binary_search_recursive(ip_hash[ip_array[i+1, last]], ip)
+                  ip_array[i+1, last].each { |k| @sub_hash_key[k] = ip_hash[k] }
+                  #return binary_search_recursive(ip_hash[ip_array[i+1, last]], ip)
               else
-                  return binary_search_recursive(ip_hash[ip_array[first, i]], ip)
+                  ip_array[first, i].each { |k| @sub_hash_key[k] = ip_hash[k] }
+                  #return binary_search_recursive(ip_hash[ip_array[first, i]], ip)
               end
+              return binary_search_recursive(@sub_hash_key, ip)
           end
       end
     end
@@ -70,8 +74,8 @@ module LogStash module Filters module FetchStrategy module File
     def fetch(source, results)
       @read_lock.lock
       begin
-        if !(IPAddr.new(source) rescue nil).nil? 
-          ip = IPAddr.new(source)
+        if !(IPAddr.new(source.strip) rescue nil).nil? 
+          ip = IPAddr.new(source.strip)
           #key = @dictionary.keys.detect{|k| @keys_cidr[k].include?(ip)}
           key = binary_search_recursive(@keys_cidr, ip)
           if key.nil?
